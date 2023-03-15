cb76a9997503f94c9f254bf301a2dd1c9c91d72b
baya <abaya6054@gmail.com>
2023-03-15 01:00:23 +0100

    start again

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 const express = require('express');
-const fs = require('fs');
-const path = require('path');
 const bodyParser = require('body-parser');
 const pdf = require('html-pdf');
 const cors = require('cors');
@@ -20,11 +18,8 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 app.post('/create-pdf', (req, res) => {
-    console.log(req.body,'helloooooooooooooooooo');
-    const timestamp = new Date().getTime();
-
-    pdf.create(pdfTemplate(req.body), {}).toFile(`result-${timestamp}.pdf`, (err) => {
-        
+    console.log(req.body,'hellooooooooooo');
+    pdf.create(pdfTemplate(req.body), {}).toFile('result.pdf', (err) => {
         if(err) {
            return res.send(Promise.reject());
         }
@@ -33,24 +28,8 @@ app.post('/create-pdf', (req, res) => {
     });
 });
 
-
-
 app.get('/fetch-pdf', (req, res) => {
-    //const files = fs.readdirSync(__dirname).filter(file => file.startsWith('result-'));
-    const files = fs.readdirSync(path.join(__dirname, '../')).filter(file => file.startsWith('result-'));
-    console.log(files,'h1111111111111');
-    const latestFile = files.sort().reverse()[0];
-    console.log(latestFile,'h2222222222222');
-
-
-    if (!latestFile) {
-        return res.status(404).send('PDF file not found');
-    }
-
-    // set cache-control header to prevent caching of the PDF file
-    res.set('Cache-Control', 'max-age=0');
-
-    res.sendFile(`${path.join(__dirname, '../')}/${latestFile}`);
-});
+    res.sendFile(`${__dirname}/result.pdf`)
+})
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
